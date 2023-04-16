0cf20a9aa50199a6cef497c1b1b8220c15216f66
GhilesLarbi <ghiles.larbi@fgei.ummto.dz>
2023-04-16 07:04:02 +0100

    minro changes

--- src/api/models/tables/game.model.js
+++ src/api/models/tables/game.model.js
@@ -1,39 +1,43 @@
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require('sequelize')
 // match moedel
 module.exports = (sequelize) => {
 	sequelize.define('game', {
-		id : {
+		id: {
 			// type : DataTypes.UUID,
-			type : DataTypes.INTEGER,
-			autoIncrement : true,
-			allowNull : false,
-			primaryKey : true,
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true,
 			// defaultValue : DataTypes.UUIDV4,
-			unique : true,
+			unique: true,
 		},
-	
-		date : {
-			type : DataTypes.DATE,
-			defaultValue : DataTypes.NOW,
+
+		date: {
+			type: DataTypes.DATE,
+			defaultValue: DataTypes.NOW,
 		},
-		
-		description : {
-			type : DataTypes.STRING,
+
+		description: {
+			type: DataTypes.STRING,
 		},
-		
-		
-		leagueId : {
-			type : DataTypes.INTEGER,
-			defaultValue : 1,
+
+
+		leagueId: {
+			type: DataTypes.INTEGER,
+			defaultValue: 1,
 			references: {
 				model: "league",
 				key: "id"
 			}
 		},
-		
-		score : {
-			type : DataTypes.STRING,
+
+		score: {
+			type: DataTypes.STRING,
 		},
-		
+
+	}, {
+		defaultScope: {
+			order: [['date', 'ASC']]
+		}
 	})
 }
--- src/api/models/tables/user.model.js
+++ src/api/models/tables/user.model.js
@@ -1,149 +1,149 @@
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require('sequelize')
 const bcrypt = require('bcryptjs')
 // user moedel
 module.exports = (sequelize) => {
 	sequelize.define('user', {
-		id : {
-			type : DataTypes.INTEGER,
-			allowNull : false,
-			primaryKey : true,
-			autoIncrement : true,
-			unique : true,
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+			unique: true,
 		},
-	
-		username : {
-			type : DataTypes.VIRTUAL,
+
+		username: {
+			type: DataTypes.VIRTUAL,
 			get() {
 				return this.getDataValue('firstname') + '_' + this.getDataValue('id')
 			}
 		},
-	
-		firstname : {
-			type : DataTypes.STRING,
-			allowNull : false,
-			validate : {
-				notEmpty : {
-					msg : 'First name cannot be empty',
+
+		firstname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: 'First name cannot be empty',
 				},
-				
-				notNull : {
-					msg : 'First name cannot be null',
+
+				notNull: {
+					msg: 'First name cannot be null',
 				},
 			},
 		},
-	
-		lastname : {
-			type : DataTypes.STRING,
-			allowNull : false,
-			validate : {
-				notEmpty : {
-					msg : 'Last name cannot be empty',
+
+		lastname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: {
+					msg: 'Last name cannot be empty',
 				},
-				
-				notNull : {
-					msg : 'Last name cannot be null',
+
+				notNull: {
+					msg: 'Last name cannot be null',
 				},
 			}
 		},
-	
-		password : {
-			type : DataTypes.STRING,
-			allowNull : false,
+
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
 			set(value) {
 				this.setDataValue('password', bcrypt.hashSync(value, 10))
 			},
-			
-			validate : {
-				notEmpty : {
-					msg : 'Password cannot be empty',
+
+			validate: {
+				notEmpty: {
+					msg: 'Password cannot be empty',
 				},
-				
-				notNull : {
-					msg : 'Password cannot be null',
+
+				notNull: {
+					msg: 'Password cannot be null',
 				},
 			},
 		},
-	
-		email : {
-			type : DataTypes.STRING,
-			allowNull : false,
-			unique : true,
-			
+
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+
 			set(value) {
 				this.setDataValue('email', value.toLowerCase())
 			},
-			
+
 			validate: {
 				isEmail: {
-					msg : "Doesn't look like an email",
+					msg: "Doesn't look like an email",
 				},
-				notEmpty : {
-					msg : 'Email cannot be empty',
+				notEmpty: {
+					msg: 'Email cannot be empty',
 				},
-				
-				notNull : {
-					msg : 'Email cannot be null',
-				},
-			},
-		},
-		
-		isEmailConfirmed : {
-			type : DataTypes.BOOLEAN,
-			allowNull : false,
-			defaultValue : false,
-		},
-		
-		phone : {
-			type : DataTypes.STRING,
-			allowNull : false,
-			unique : true,
-			validate: {
-				isNumeric : {
-					msg : "Doesn't look like a phone number",
-				},
-				
-				notEmpty : {
-					msg : 'Phone number cannot be empty',
-				},
-				
-				notNull : {
-					msg : 'Phone number cannot be null',
+
+				notNull: {
+					msg: 'Email cannot be null',
 				},
 			},
 		},
-		
-		nationalNumber :{
-			type : DataTypes.STRING,
-			allowNull : false,
-			unique : true,
+
+		isEmailConfirmed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+
+		phone: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
 			validate: {
-				isNumeric : {
-					msg : "Doesn't look like a national number",
+				isNumeric: {
+					msg: "Doesn't look like a phone number",
 				},
-				notEmpty : {
-					msg : 'National number cannot be empty',
+
+				notEmpty: {
+					msg: 'Phone number cannot be empty',
 				},
-				
-				notNull : {
-					msg : 'National number cannot be null',
+
+				notNull: {
+					msg: 'Phone number cannot be null',
+				},
+			},
+		},
+
+		nationalNumber: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isNumeric: {
+					msg: "Doesn't look like a national number",
+				},
+				notEmpty: {
+					msg: 'National number cannot be empty',
+				},
+
+				notNull: {
+					msg: 'National number cannot be null',
 				},
 			},
 		},
 	}, {
-		defaultScope : {
-			attributes : {
-				exclude : ['password'],
+		defaultScope: {
+			attributes: {
+				exclude: ['password'],
 			},
 		},
-		
-		scopes : {
+
+		scopes: {
 			login(email) {
 				return {
-					attributes : ['password', 'email', 'id'],
-					where : {
-						email : email,
+					attributes: ['password', 'email', 'id'],
+					where: {
+						email: email,
 					},
-					raw : true,
+					raw: true,
 				}
 			},
 		},
